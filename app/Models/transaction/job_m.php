52e98114f10346c6eb022e1542a063a42f62d3f9
ihsandulu <ihsan.dulu@gmail.com>
2026-05-12 20:35:51 +0700

    bug upload picture di job

--- app/Models/transaction/job_m.php
+++ app/Models/transaction/job_m.php
@@ -146,7 +146,7 @@ class job_m extends core_m
                 }
             } else {
                 // File Tipe Tidak Sesuai
-                $data['uploadjob_picture'] = "Format File Salah !";
+                $data['uploadjob_picture'] = "Format File Salah !".$type;
             }
         }
         // dd($_POST);
